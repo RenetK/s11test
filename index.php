cb966fc466a2f1ae06d2ac1e171fb430dbde6016
RenetK <115539940+RenetK@users.noreply.github.com>
2023-10-16 10:41:49 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Test veebileht!</title>
-    <meta name="description" content="A brief description of your website">
+    <meta name="description" content="<img src='https://www.colorado.edu/linguistics/sites/default/files/styles/small/public/article-image/kenobi-thumb.png?itok=si_nqtPv' alt='Hello There'>">
     <meta name="keywords" content="keywords, separated, by, commas">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="styles.css">
