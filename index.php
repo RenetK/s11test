4f1a14011c3ae1479d3e0dfff48b4fc906e97291
RenetK <115539940+RenetK@users.noreply.github.com>
2023-10-16 10:33:36 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,35 @@
+<!DOCTYPE html>
 <html>
-  <H1>test leht ok!!!!!!!!!!!!!!</H1>
+<head>
+    <meta charset="UTF-8">
+    <title>Test veebileht!</title>
+    <meta name="description" content="A brief description of your website">
+    <meta name="keywords" content="keywords, separated, by, commas">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+
+<header>
+    <h1>Yest!!!!!!!!!!!!!!!!!!!!!!!!!!!</h1>
+    <nav>
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="about.html">About</a></li>
+            <li><a href="contact.html">Contact</a></li>
+        </ul>
+    </nav>
+</header>
+
+<main>
+    <h2>Test website</h2>
+    <p>Renet Test website</p>
+</main>
+
+<footer>
+    <p>&copy; 2023 Test Website</p>
+</footer>
+
+</body>
 </html>
+
